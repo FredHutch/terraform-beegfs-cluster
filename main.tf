544f35b38bbea8820672af3dc102b0d3e7d1307a
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-11-29 12:39:42 -0800

    Add stanza for metadata volumes

--- main.tf
+++ main.tf
@@ -5,12 +5,30 @@ provider "aws" {
 # Create volumes for datastore
 
 resource "aws_ebs_volume" "data_volumes" {
-  availability_zone = "${var.az}"
   count = "${var.data_volume["nvols"]}"
+
+  availability_zone = "${var.az}"
   size = "${var.data_volume["size"]}"
   type = "${var.data_volume["type"]}"
+
   tags = "${merge(
-    map("Name", "${var.cluster_name}_vol${count.index}"),
+    map("Name", "${var.cluster_name}_stor${count.index}"),
+    var.tags
+  )}"
+
+}
+
+# Create metadata volumes
+resource "aws_ebs_volume" "metadata_volumes" {
+  count = "${var.md_node["nnodes"]}"
+
+  availability_zone = "${var.az}"
+  size = "${var.md_data["size"]}"
+  type = "${var.md_data["type"]}"
+  iops = "${var.md_data["iops"]}"
+
+  tags = "${merge(
+    map("Name", "${var.cluster_name}_md${count.index}"),
     var.tags
   )}"
 }
